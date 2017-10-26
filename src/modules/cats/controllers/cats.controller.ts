365ce248042c92185366999d00846bf5ff3ebb65
Ovidiu Popoviciu <ovidiup13@tuta.io>
2017-10-26 01:42:30 +0100

    Forbid access to PUT method

--- src/modules/cats/controllers/cats.controller.ts
+++ src/modules/cats/controllers/cats.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get, Req, Post, Body, HttpStatus, Res } from '@nestjs/common';
+import { Controller, Get, Req, Post, Body, HttpStatus, Res, Put } from '@nestjs/common';
+import { HttpException } from '@nestjs/core';
+
 import { CreateCatDto } from '../dto/create-cat.dto';
 import { Cat } from '../interfaces/cat.interface';
 import { CatsService } from '../components/cats.service';
@@ -18,4 +20,11 @@ export class CatsController {
     async create( @Body() createCatDto: CreateCatDto) {
         this.catsService.create(createCatDto);
     }
+
+    @Put()
+    async update( @Body() createCatDto: CreateCatDto) {
+        // can override response body by passing an object instead of the string 'Forbidden'
+        throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    }
+
 }
